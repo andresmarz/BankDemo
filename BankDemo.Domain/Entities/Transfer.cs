55387990a1e847f82ded86f40e690992da841c88
Andres Marquez Zuleta <andresmarquezzuleta@gmail.com>
2026-01-05 18:34:01 -0400

    Add Transfer domain entity with basic business rules

---
+++ BankDemo.Domain/Entities/Transfer.cs
@@ -0,0 +1,56 @@
+﻿using BankDemo.Domain.Enums;
+using BankDemo.Domain.Exceptions;
+
+namespace BankDemo.Domain.Entities;
+
+public class Transfer
+{
+    public Guid Id { get; }
+    public string CustomerId { get; }
+    public string FromAccountId { get; }
+    public Guid BeneficiaryId { get; }
+    public decimal Amount { get; }
+    public Currency Currency { get; }
+    public string? Description { get; }
+    public TransferStatus Status { get; private set; }
+    public string ClientRequestId { get; }
+    public DateTime CreatedAt { get; }
+
+    public Transfer(
+        Guid id,
+        string customerId,
+        string fromAccountId,
+        Guid beneficiaryId,
+        decimal amount,
+        Currency currency,
+        string? description,
+        string clientRequestId,
+        DateTime createdAt)
+    {
+        if (string.IsNullOrWhiteSpace(customerId))
+            throw new DomainException("Customer id is required.");
+
+        if (string.IsNullOrWhiteSpace(fromAccountId))
+            throw new DomainException("From account id is required.");
+
+        if (amount <= 0)
+            throw new DomainException("Transfer amount must be greater than zero.");
+
+        if (!string.IsNullOrWhiteSpace(description) && description.Length > 140)
+            throw new DomainException("Description max length is 140 characters.");
+
+        if (string.IsNullOrWhiteSpace(clientRequestId))
+            throw new DomainException("ClientRequestId is required.");
+
+        Id = id;
+        CustomerId = customerId;
+        FromAccountId = fromAccountId;
+        BeneficiaryId = beneficiaryId;
+        Amount = amount;
+        Currency = currency;
+        Description = description;
+        ClientRequestId = clientRequestId;
+        CreatedAt = createdAt;
+        Status = TransferStatus.Completed; // simplificado
+    }
+}
--- BankDemo.Domain/Enums/TransferStatus.cs
+++ BankDemo.Domain/Enums/TransferStatus.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BankDemo.Domain.Enums;
 
-namespace BankDemo.Domain.Enums
+public enum TransferStatus
 {
-    internal class TransferStatus
-    {
-    }
+    Pending,
+    Completed,
+    Rejected
 }
