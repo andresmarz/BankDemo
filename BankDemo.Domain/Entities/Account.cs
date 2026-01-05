605e719e07f4b8f278ece0789aa67ca9031ae545
Andres Marquez Zuleta <andresmarquezzuleta@gmail.com>
2026-01-05 18:17:49 -0400

    Add Account domain entity

---
+++ BankDemo.Domain/Entities/Account.cs
@@ -0,0 +1,29 @@
+﻿using BankDemo.Domain.Enums;
+using BankDemo.Domain.Exceptions;
+
+namespace BankDemo.Domain.Entities;
+
+public class Account
+{
+    public string Id { get; }
+    public string CustomerId { get; }
+    public Currency Currency { get; }
+    public decimal AvailableBalance { get; }
+
+    public Account(string id, string customerId, Currency currency, decimal availableBalance)
+    {
+        if (string.IsNullOrWhiteSpace(id))
+            throw new DomainException("Account id is required.");
+
+        if (string.IsNullOrWhiteSpace(customerId))
+            throw new DomainException("Customer id is required.");
+
+        if (availableBalance < 0)
+            throw new DomainException("Available balance cannot be negative.");
+
+        Id = id;
+        CustomerId = customerId;
+        Currency = currency;
+        AvailableBalance = availableBalance;
+    }
+}
