6f5ca91e762c56b7b1109477cd034454a1daf56a
Andres Marquez Zuleta <andresmarquezzuleta@gmail.com>
2026-01-05 18:13:48 -0400

    Add base domain exception

--- BankDemo.Domain/Enums/TransferStatus.cs
+++ BankDemo.Domain/Enums/TransferStatus.cs
@@ -1,8 +1,12 @@
-﻿namespace BankDemo.Domain.Enums;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public enum TransferStatus
+namespace BankDemo.Domain.Enums
 {
-    Pending,
-    Completed,
-    Rejected
+    internal class TransferStatus
+    {
+    }
 }
---
+++ BankDemo.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,8 @@
+﻿namespace BankDemo.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException(string message) : base(message)
+    {
+    }
+}
