d99e386aea0e94fbc43897baa683e830a91665ff
Andres Marquez Zuleta <andresmarquezzuleta@gmail.com>
2026-01-05 23:10:43 -0400

    Add API DTOs for transfer creation

---
+++ BankDemo.API/DTOs/Transfers/CreateTransferRequest.cs
@@ -0,0 +1,14 @@
+﻿using BankDemo.Domain.Enums;
+
+namespace BankDemo.API.DTOs.Transfers;
+
+public class CreateTransferRequest
+{
+    public string CustomerId { get; set; } = null!;
+    public string ClientRequestId { get; set; } = null!;
+    public string FromAccountId { get; set; } = null!;
+    public Guid BeneficiaryId { get; set; }
+    public decimal Amount { get; set; }
+    public Currency Currency { get; set; }
+    public string? Description { get; set; }
+}
---
+++ BankDemo.API/DTOs/Transfers/CreateTransferResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BankDemo.API.DTOs.Transfers;
+
+public class CreateTransferResponse
+{
+    public Guid TransferId { get; set; }
+    public string Status { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
