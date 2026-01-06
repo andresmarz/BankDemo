32446ba0a722cdf24489cb4a3553619de457788b
Andres Marquez Zuleta <andresmarquezzuleta@gmail.com>
2026-01-05 21:53:17 -0400

    Add SQL Server connection factory using Dapper

---
+++ BankDemo.Infrastructure/Data/Dapper/SqlConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace BankDemo.Infrastructure.Data.Dapper;
+
+public class SqlConnectionFactory
+{
+    private readonly IConfiguration _configuration;
+
+    public SqlConnectionFactory(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        return new SqlConnection(connectionString);
+    }
+}
